47334446dad235ccdd81a1cf1d3ec5e060aeee43
livferz <liviafernandesfaria@gmail.com>
2024-09-24 07:44:23 -0300

    react

---
+++ sgpl-new/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+       <Navbar/>
+       
+       </div>
+  )
+
+
+}
+
+
+
+export default App
