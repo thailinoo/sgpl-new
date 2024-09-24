2b758bbfbb2244bba8ba8a60102ad9f31f610efd
livferz <liviafernandesfaria@gmail.com>
2024-09-24 09:47:09 -0300

    new

--- sgpl-new/src/Components/Navbar.jsx
+++ sgpl-new/src/Components/Navbar.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 import './Navbar.css'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <><header className='header'>
+      <a href='/' className='logo'>Logo</a>
+    </header><nav className='navbar'>
+        <a href='/'>Solicitações</a>
+        <a href='/'>Devoluções</a>
+        <a href='/'>Nova Ocorrência</a>
+        <a href='/'>Sair</a>
+      </nav></>
   )
 
 
