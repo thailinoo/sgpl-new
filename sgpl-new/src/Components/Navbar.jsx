24b13ff33f6e8d7990c9a43075ffc6b8a4ce0963
livferz <liviafernandesfaria@gmail.com>
2024-09-25 07:59:01 -0300

    novo

--- sgpl-new/src/Components/Navbar.jsx
+++ sgpl-new/src/Components/Navbar.jsx
@@ -4,12 +4,13 @@ const Navbar = () => {
   return (
     <><header className='header'>
       <a href='/' className='logo'>Logo</a>
-    </header><nav className='navbar'>
+    <nav className='navbar'>
         <a href='/'>Solicitações</a>
         <a href='/'>Devoluções</a>
         <a href='/'>Nova Ocorrência</a>
         <a href='/'>Sair</a>
-      </nav></>
+      </nav></header></>
+     
   )
 
 
